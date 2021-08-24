dd7c8a685f48fb2da8b5652e870058e067779435
Alice De Schutter <alice@mailfence.com>
2021-08-24 15:20:20 +0200

    MLflow setup

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -2,6 +2,12 @@ from TaxiFareModel.pipeline import TaxifarePipeline
 from TaxiFareModel.utils import compute_rmse
 from TaxiFareModel.data import get_data, clean_data
 from sklearn.model_selection import train_test_split
+from memoized_property import memoized_property
+import mlflow
+from mlflow.tracking import MlflowClient
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = "[SE] [Stockholm] [alicedeschutter] linear + 1"
 
 class Trainer():
     def __init__(self, X, y):
@@ -12,6 +18,7 @@ class Trainer():
         self.pipeline = None
         self.X = X
         self.y = y
+        self.experiment_name = EXPERIMENT_NAME
 
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -26,7 +33,35 @@ class Trainer():
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipe.predict(X_test)
         RMSE = compute_rmse(y_pred, y_test)
+        self.mlflow_log_metric('rmse', RMSE)
         return RMSE
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
+
+    def save_model(self):
+        """ Save the trained model into a model.joblib file """
+        pass
+
 
 if __name__ == "__main__":
 
@@ -50,3 +85,5 @@ if __name__ == "__main__":
     # evaluate
 
     print(trainer.evaluate(X_val, y_val))
+    experiment_id = trainer.mlflow_experiment_id
+    print(f"experiment URL: https://mlflow.lewagon.co/#/experiments/{experiment_id}")
